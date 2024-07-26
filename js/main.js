1b4a174a3e4411a92f2c0b0f8c2ea6b128717ee2
afed1987 <afed1987@gmail.com>
2024-07-26 09:37:39 +0300

    Add files via upload
    Signed-off-by: afed1987 <afed1987@gmail.com>

---
+++ js/main.js
@@ -0,0 +1,10 @@
+import '../dev/scss/_reset.scss'
+import '../dev/scss/style.scss'
+
+import Logo from '/dev/img/logo.svg'
+import person from '/dev/img/person.png'
+
+document.querySelector('.logo img').src=`${Logo}`
+document.querySelector('.manager__icon img').src=`${person}`
+
+
